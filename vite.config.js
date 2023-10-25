4a632bd54173d6b60491e901b93e19960d540dfe
jiumingmao <879021430@qq.com>
2023-10-25 20:07:19 +0800

    build: 🛠️ 配置自动导入el-icon

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,10 +17,30 @@ export default defineConfig({
     vueJsx(),
     // ElementPlus自动按需导入
     AutoImport({
-      resolvers: [ElementPlusResolver()]
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon'
+        })
+      ]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep']
+        }),
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver()
+      ]
+    }),
+    Icons({
+      autoInstall: true
     })
   ],
   resolve: {
